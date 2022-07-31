60c700fc773cc5556b273b06deb85f8859e2cf18
jongkeun-kim <kjkandrea@gmail.com>
2022-07-31 14:49:08 +0900

    feat. add Datetime add/subtract

--- src/apis/Datetime.ts
+++ src/apis/Datetime.ts
@@ -1,20 +1,30 @@
 import moment from "moment";
 
+type ConstructDate = Date | number
 type DateFormat = 'YYYY-MM-DD' | 'LTS'
+type DateUnit = 'day' | 'month' | 'year'
 
 class Datetime {
 	private _date: Date;
 
-	constructor(date = new Date()) {
-		this._date = date
+	constructor(date: ConstructDate = new Date()) {
+		this._date = date instanceof Date ? date : new Date(date)
 	}
 
-	static datetime(date = new Date()) {
+	static datetime(date: ConstructDate = new Date()) {
 		return new Datetime(date)
 	}
 
 	public format(dateFormat: DateFormat) {
 		return moment(this._date).format(dateFormat)
+	}
+
+	public add(amount: number, unit: DateUnit) {
+		return Datetime.datetime(moment(this._date).add(amount, unit).toDate())
+	}
+
+	public subtract(amount: number, unit: DateUnit) {
+		return Datetime.datetime(moment(this._date).subtract(amount, unit).toDate())
 	}
 }
 
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import {datetime} from "./apis/Datetime";
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Datetime Wrapper</h1>
-    <p class="read-the-docs">
+    <p>
       now YMD : ${datetime().format('YYYY-MM-DD')}
+    </p>
+    <p>
+      tummorrow YMD : ${datetime().add(1, 'year').format('YYYY-MM-DD')}
+    </p>
+    <p>
+      1 year ago YMD : ${datetime().subtract(1, 'year').format('YYYY-MM-DD')}
     </p>
   </div>
 `
